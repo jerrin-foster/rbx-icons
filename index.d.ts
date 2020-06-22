c8552819032871a3f698e2ecf8f58b145a3105ff
Muoshuu <admin@muoshuu.me>
2020-06-22 10:18:54 -0500

    Add iconIndex param

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,8 @@ export declare type ClassIconPaths = {
     [className: string]: string
 }
 
-export declare function generate(outputDir: string): Promise<ClassIconPaths>
+export declare type ClassIconIndex = {
+    [className: string]: string
+}
+
+export declare function generate(outputDir: string, iconIndex: ClassIconIndex): Promise<ClassIconPaths>
--- index.js
+++ index.js
@@ -8,15 +8,13 @@ const slice = require('./slice');
 const getJSON = bent('json');
 const getBuffer = bent('buffer');
 
-function generate(outputDir) {
+function generate(outputDir, iconIndex) {
     return new Promise((resolve, reject) => {
         if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
 
-        let sheetPath = path.join(outputDir, 'sheet.png');
-
         Promise.all([
             getJSON('https://raw.githubusercontent.com/RobloxAPI/build-archive/master/data/production/latest.json'),
-            getJSON('https://reflection.rbx-api.xyz/icons')
+            iconIndex ? Promise.resolve(iconIndex) : getJSON('https://reflection.rbx-api.xyz/icons') // Reflection API relies on this module, but has its own icon index
         ]).then(response => {
             let version = response[0].GUID;
             let index = response[1];
